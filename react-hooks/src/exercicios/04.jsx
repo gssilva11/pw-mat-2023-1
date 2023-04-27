9a2b968c60dc983e6b87ca94062f99dfa83f2fb0
Fausto G. Cintra <professor@faustocintra.com.br>
2023-04-27 09:02:34 -0300

    (27/04) Exercícios 04 e 05

--- react-hooks/src/exercicios/04.jsx
+++ react-hooks/src/exercicios/04.jsx
@@ -3,7 +3,21 @@ import * as React from 'react'
 function Board() {
   // 🐨 squares é o estado para este componente. Adicione useState para squares
   //const squares = Array(9).fill(null)
-  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  
+  // Quando o componente for carregado, verificamos se existe estado salvo
+  // e inicializamos a variável de estado com isso.
+  // Como o estado salvo é string e o nossa variável de estado é vetor,
+  // é necessário converter de um para o outro usando JSON.parse()
+  const [squares, setSquares] = React.useState(
+    // Usa o estado gravado no localStorage, se houver, ou um vetor de
+    // 9 nulos, caso contrário
+
+    // Fornecendo uma função em vez de um valor, o React entenderá que7
+    // queremos executar a ação de inicialização do estado apenas durante
+    // a fase "mount" do ciclo de vida do componente, o que é chamado
+    // "lazy initializer"
+    () => JSON.parse(window.localStorage.getItem('tic-tac-toe')) ?? Array(9).fill(null)
+  )
 
   // 🐨 Precisaremos dos seguintes itens de estados derivados:
   // - nextValue ('X' ou 'O')
@@ -14,6 +28,12 @@ function Board() {
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
+
+  React.useEffect(() => {
+    // Como o estado "squares" é um vetor, ele deve ser convertido em
+    // string com JSON.stringify() antes de ser salvo no localStorage
+    window.localStorage.setItem('tic-tac-toe', JSON.stringify(squares))
+  }, [squares])
 
   // Esta é a função que o manipulador de clique no quadrado irá chamar. `square`
   // deve ser um índice. Portanto, se você clicar sobre o quadrado central, o
@@ -78,7 +98,6 @@ function Board() {
         restart
       </button>
       <hr />
-      <div>{ JSON.stringify(squares) }</div>
     </div>
   )
 }
