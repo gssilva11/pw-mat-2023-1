21a4154efcf1a8d242d957299f6fbcbe26e87887
gssilva11 <132019814+gssilva11@users.noreply.github.com>
2023-05-04 12:11:52 +0000

    (04/05) Início do projeto karangos

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -2,16 +2,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { BrawserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import TopBar from './components/ui/TopBar'
 
 function App() {
 
   return (
     <>
-      <BrawserRouter>
+      <BrowserRouter>
         <TopBar/>
-      </BrawserRouter>
+      </BrowserRouter>
     </>
   )
 }
